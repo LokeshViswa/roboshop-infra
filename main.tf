7d6382496e7e84d2af32173a24e602e6bfb1cb0c
LokeshV <lokeshviswa428@gmail.com>
2023-02-24 07:58:45 +0530

    Roboshop Implementation in Terraform

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ module "network" {
   env    = var.env
   default_vpc_id = var.default_vpc_id
 
-  for_each     = var.vpc
-  cidr_block   = each.value.cidr_block
-  subnets_cidr = each.value.subnets_cidr
+  for_each             = var.vpc
+  cidr_block           = each.value.cidr_block
+  public_subnets_cidr  = each.value.public_subnets_cidr
+  private_subnets_cidr = each.value.private_subnets_cidr
 }
 
 #module "vpc" {
