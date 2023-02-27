3fac7ab9dfcd031f4f0289ad2f58bbad7139f776
LokeshV <lokeshviswa428@gmail.com>
2023-02-27 21:34:54 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ module "vpc" {
 
   for_each             = var.vpc
   cidr_block           = each.value.cidr_block
-  public_subnet_ids = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
 }
 
 module "subnets" {
@@ -19,14 +18,13 @@ module "subnets" {
   name              = each.value.name
   vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
   vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
-  internet_gw_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "internet_gw_id", null)
   internet_gw               = lookup(each.value, "internet_gw", false)
   nat_gw                    = lookup(each.value, "nat_gw", false)
 }
 
-output "subnet_ids" {
-  value = module.subnets
-}
+#output "subnet_ids" {
+#  value = module.subnets
+#}
 
 #module "vpc" {
 #  source         = "github.com/raghudevopsb70/tf-module-vpc"
