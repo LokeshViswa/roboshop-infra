3dc40dcf77df843bdbf14aff7aee8ce12a45c562
LokeshV <lokeshviswa428@gmail.com>
2023-02-27 18:28:50 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -12,12 +12,15 @@ module "subnets" {
   env            = var.env
   default_vpc_id = var.default_vpc_id
 
-  vpc_id = module.vpc.vpc_id
-
   for_each          = var.subnets
   cidr_block        = each.value.cidr_block
   availability_zone = each.value.availability_zone
   name              = each.value.name
+  vpc_id            = lookup(module.vpc, each.value.vpc_name, null)
+}
+
+output "vpc_id" {
+  value = module.vpc
 }
 
 #module "vpc" {
