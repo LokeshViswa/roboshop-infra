c807400961db441f9e0a472097004cb542cd51c1
LokeshV <lokeshviswa428@gmail.com>
2023-03-14 19:52:30 +0530

    Roboshop Implementation in Terraform

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ module "apps" {
   source = "github.com/LokeshViswa/tf-module-app"
   env    = var.env
 
-  depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds]
+  depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
 
   for_each         = var.apps
   subnet_ids       = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
@@ -97,6 +97,6 @@ module "apps" {
   bastion_cidr     = var.bastion_cidr
 }
 
-#output "vpc" {
-#  value = lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnets", null), "public", null), "subnet_ids", null)
-#}
+output "redis" {
+  value = module.elasticache
+}
